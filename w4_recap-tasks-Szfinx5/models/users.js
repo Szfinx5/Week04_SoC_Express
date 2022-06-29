64519500b6bd56e5c938a982d0bb755dc3759b28
ghavasi <g.havasi@outlook.com>
2022-06-29 21:02:39 +0100

    Recap task routes are working

--- w4_recap-tasks-Szfinx5/models/users.js
+++ w4_recap-tasks-Szfinx5/models/users.js
@@ -1,16 +1,66 @@
-import {users} from "../libs/users.js";
+import { users } from "../libs/users.js";
 
 // GET ALL USERS
-export function getUsers() {}
+export function getUsers() {
+  const responseObject = {
+    success: true,
+    payload: users,
+  };
+  return responseObject;
+}
 
 // GET A USER BY ID
-export function getUserByID(id) {}
+export function getUserByID(id) {
+  let userById = {};
+  userById = users.filter((r) => r.id == id);
+  const responseObject = {
+    success: true,
+    payload: userById,
+  };
+  return responseObject;
+}
 
 // CREATE A USER
-export function createUser(newUser) {}
+export function createUser(newUser) {
+  users.push(newUser);
+
+  const responseObject = {
+    success: true,
+    payload: newUser,
+  };
+  return responseObject;
+}
 
 // UPDATE A USER BY ID
-export function updateUserByID(id, updatedUser) {}
+export function updateUserByID(id, updatedUser) {
+  for (let i = 0; i < users.length; i++) {
+    if (id == users[i].id) {
+      users[i] = updatedUser;
+      break;
+    }
+  }
+  const responseObject = {
+    success: true,
+    payload: updatedUser,
+  };
+  return responseObject;
+}
 
 // DELETE A USER BY ID
-export function deleteUserByID(id) {}
+export function deleteUserByID(id) {
+  let deletedUser = users.filter((r) => r.id == id);
+
+  for (let i = 0; i < users.length; i++) {
+    if (id == users[i].id) {
+      users.splice(i, 1);
+      break;
+    }
+  }
+
+  console.log(deletedUser);
+  const responseObject = {
+    success: true,
+    payload: `User ${deletedUser.params.id} is deleted`,
+  };
+  return responseObject;
+}
--- w4_recap-tasks-Szfinx5/routes/users.js
+++ w4_recap-tasks-Szfinx5/routes/users.js
@@ -1,83 +1,42 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
+import { users } from "../libs/users.js";
+import {
+  getUsers,
+  getUserByID,
+  createUser,
+  updateUserByID,
+  deleteUserByID,
+} from "../models/users.js";
 
-import {users} from '../libs/users.js';
-
-//Get all users 
-router.get('/', function(req, res){
-    const responseObject = {
-        success: true, 
-        payload: users, 
-    };
-    res.json(responseObject)
+//Get all users
+router.get("/", function (req, res) {
+  res.json(getUsers());
 });
-
 
 //Get user by id
-router.get('/:id', function(req, res){
-    const id = req.params.id;
-    let userById = {};
-    userById = users.filter(r => r.id == id);
-    
-    const responseObject = {
-        success: true, 
-        payload: userById, 
-    };
-    res.json(responseObject)
+router.get("/:id", function (req, res) {
+  const id = req.params.id;
+  res.json(getUserByID(id));
 });
-
 
 //Create new user
-router.post('/', function(req, res){
-    const body = req.body;
-    users.push(body);
- 
-    const responseObject = {
-        success: true, 
-        payload: body 
-    };
-    res.json(responseObject);
+router.post("/", function (req, res) {
+  const body = req.body;
+  res.json(createUser(body));
 });
-
 
 //Update user by id
-router.put('/:id', function(req, res){
-    const id = req.params.id;
-    
-    for(let i=0;i<users.length;i++){
-        console.log(i);
-        if(id == users[i].id){
-            users[i] = req.body;
-            break;
-        };
-    };
-    const responseObject = {
-        success: true, 
-        payload: req.body 
-    };
-    res.json(responseObject);
+router.put("/:id", function (req, res) {
+  const id = req.params.id;
+  const body = req.body;
+  res.json(updateUserByID(id, body));
 });
 
-
 //Delete user by id
-router.delete('/:id', function (req, res){
-    const id = req.params.id;
-    
-    let deletedUser = users.filter(r => r.id == id);
-    
-    for(let i=0;i<users.length;i++){
-        if(id == users[i].id){
-            users.splice(i,1);
-            break;
-        };
-    };
-    
-    console.log(deletedUser);
-    const responseObject = {
-        success: true, 
-        payload: `User ${req.params.id} is deleted`
-    };
-    res.json(responseObject);
-})
+router.delete("/:id", function (req, res) {
+  const id = req.params.id;
+  res.json(deleteUserByID(id));
+});
 
-export {router};
+export { router };
